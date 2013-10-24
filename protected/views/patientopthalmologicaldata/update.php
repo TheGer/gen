2e45fdacb66bebff2873207534565889b838bb37
Gerry Said <gerrysaid@gmail.com>
2013-10-24 07:37:46 +0200

    ..

--- protected/views/patientopthalmologicaldata/update.php
+++ protected/views/patientopthalmologicaldata/update.php
@@ -3,19 +3,19 @@
 /* @var $model Patientopthalmologicaldata */
 
 $this->breadcrumbs=array(
-	'Patientopthalmologicaldatas'=>array('index'),
+	'Patient Phenotype'=>array('index'),
 	$model->id=>array('view','id'=>$model->id),
 	'Update',
 );
 
 $this->menu=array(
-	array('label'=>'List Patientopthalmologicaldata', 'url'=>array('index')),
-	array('label'=>'Create Patientopthalmologicaldata', 'url'=>array('create')),
-	array('label'=>'View Patientopthalmologicaldata', 'url'=>array('view', 'id'=>$model->id)),
-	array('label'=>'Manage Patientopthalmologicaldata', 'url'=>array('admin')),
+	array('label'=>'List Patient Phenotypes', 'url'=>array('index')),
+	array('label'=>'Create Patient Phenotype', 'url'=>array('create')),
+	array('label'=>'View Patient Phenotype', 'url'=>array('view', 'id'=>$model->id)),
+	array('label'=>'Manage Patient Phenotypes', 'url'=>array('admin')),
 );
 ?>
 
-<h1>Update Patientopthalmologicaldata <?php echo $model->id; ?></h1>
+<h1>Update Patient Phenotype <?php echo $model->id; ?></h1>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
